79348f284f67620ee2fd9e731873db7ced4134cb
Tetiana575 <lukenya575@gmail.com>
2025-05-05 20:42:24 +0200

    убрала коммент

--- generate-sales-data.py
+++ generate-sales-data.py
@@ -15,18 +15,18 @@ today = date.today()  # получаем текущую дату
 yesterday = today - timedelta(days=1)  # отнимаем один день
 is_weekend = yesterday.weekday() > 4  # проверяем рабочий или не рабочий день
 
-#if 1 <= today.weekday() <= 5: # если текущий день недели от 1 до 5 (понедельник-пятница)
-d = {
-    'date': [yesterday.strftime('%d-%m-%Y')] * len(COMPANIES) * 2,  # добавляем вчерашнюю дату, умножаем на количество компаний * 2 для buy и sell
-    'company': [comp for comp in COMPANIES for _ in range(2)],  # повторяем каждую компанию 2 раза
-    'transaction_type': ['buy'] * len(COMPANIES) + ['sell'] * len(COMPANIES),  # тип транзакции: покупка или продажа
-    # Генерируем случайные цены между 0 и 100000 для каждой транзакции
-    'price': [randint(0, 100000) for _ in range(len(COMPANIES) * 2)],
-    'volume': [randint(0, 1000) for _ in range(len(COMPANIES) * 2)],  # количество акций
-    'is_weekend': [is_weekend] * (len(COMPANIES) * 2)  # статус выходного дня для каждой транзакции
-}
+if 1 <= today.weekday() <= 5: # если текущий день недели от 1 до 5 (понедельник-пятница)
+    d = {
+        'date': [yesterday.strftime('%d-%m-%Y')] * len(COMPANIES) * 2,  # добавляем вчерашнюю дату, умножаем на количество компаний * 2 для buy и sell
+        'company': [comp for comp in COMPANIES for _ in range(2)],  # повторяем каждую компанию 2 раза
+        'transaction_type': ['buy'] * len(COMPANIES) + ['sell'] * len(COMPANIES),  # тип транзакции: покупка или продажа
+        # Генерируем случайные цены между 0 и 100000 для каждой транзакции
+        'price': [randint(0, 100000) for _ in range(len(COMPANIES) * 2)],
+        'volume': [randint(0, 1000) for _ in range(len(COMPANIES) * 2)],  # количество акций
+        'is_weekend': [is_weekend] * (len(COMPANIES) * 2)  # статус выходного дня для каждой транзакции
+    }
 
-df = pd.DataFrame(d)  # создаем датафрейм
-df.to_csv('sales_data.csv', index=False)  # сохраняем датафрейм в csv
+    df = pd.DataFrame(d)  # создаем датафрейм
+    df.to_csv('sales_data.csv', index=False)  # сохраняем датафрейм в csv
 
 
